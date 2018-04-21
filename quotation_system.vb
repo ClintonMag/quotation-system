d3955824b4025ebffd82337ca651d527f0cf48b7
Clinton Magano <code.clinton.magano@gmail.com>
2018-04-21 13:52:17 +0200

    Refactor to use fewer global variables

--- quotation_system.vb
+++ quotation_system.vb
@@ -2,13 +2,15 @@ REM  *****  BASIC  *****
 
 REM QuotationSystem
 
-REM Read quotation/invoice number from a 'QINUM' sheet
+REM Read quotation/invoice number from the 'QINUM' sheet
 REM Update it for the current quotation/invoice
 REM Place new quotation/invoice number in the correct cell
 REM Write new quotation/invoice, date, and client name to the
 REM 'QINUM.csv' file
 
 Option Explicit
+
+' ***** GLOBAL VARIABLES ***** '
 
 'Row and Column position of the quotation/invoice number in respective sheet
 'Row 1 in sheet = row 1 in this code
@@ -28,59 +30,63 @@ Dim AllSheets As Object
 'The applicable working sheet, either 'Quotation' or 'Invoice'
 Dim QISheet As Object
 
-'Cell containing the quotation or invoice number
+'Cell to receive the new quotation or invoice number
 Dim QICell as Object
-
-'Quotation/Invoice Number
-Dim QINum As Long
 
 'Store the full quotation number as a string
 Dim QIString As String
 
-'New Quotation/Invoice Number
-Dim NewQINum as Long
-
-'Indicate whether we are working on a quotation or an invoice
-'0 for quotation, 1 for invoice
-Dim QuoteOrInv as Integer
-
 'Index of the sheet named 'Quotation' in the ThisComponent.Sheets list
 Dim QSheetIndex as Integer
 
+
+' ***** FUNCTIONS ***** '
 
 Function Cells(sheet As Object, x As Integer, y As Integer) As Object
 	REM Simplify calls to retrieve cell position by coordinates
 	
 	Cells = sheet.getCellByPosition(y-1, x-1)
 End Function
+
+
+' ***** SUBROUTINES ***** '
 	
 Sub MainQuotation
 	REM Entry point if working on a quotation
 	
+	'Indicate whether we are working on a quotation or an invoice
+	'0 for quotation, 1 for invoice
+	Dim QuoteOrInv as Integer
+	
 	QuoteOrInv = 0
-	MainProcess
+	MainProcess(QuoteOrInv)
 
 End Sub
 
 Sub MainInvoice
 	REM Entry point if working on an invoice
+
+	'Indicate whether we are working on a quotation or an invoice
+	'0 for quotation, 1 for invoice
+	Dim QuoteOrInv as Integer
 	
 	QuoteOrInv = 1
-	MainProcess
+	MainProcess(QuoteOrInv)
 
 End Sub
 
 
-Sub MainProcess
+Sub MainProcess(QuoteOrInv)
 	REM Start the main invoice/quotation nunmber update procedure once it's
 	REM which is to be updated
 	
-	InitializeGlobals
-	GenerateQINumber
+	InitializeGlobals(QuoteOrInv)
+	GenerateQINumber(QuoteOrInv)
+	WriteQINumber(QuoteOrInv)
 
 End Sub
 
-Sub InitializeGlobals
+Sub InitializeGlobals(QuoteOrInv As Integer)
 	REM Initializes the global variables
 	
 	Doc = ThisComponent
@@ -99,15 +105,19 @@ Sub InitializeGlobals
 	
 End Sub
 
-Sub GenerateQINumber
+Sub GenerateQINumber(QuoteOrInv As Integer)
 	REM Read the last quotation/invoice number, increase it by 1 and place it
 	REM in the correct cell
 	
+	Dim i as Integer
 	'String format to be used by the Format function to specify number of zeros
 	'for left-padding
 	Dim QINumFormatString As String
-	Dim i as Integer
-	
+	'Quotation/Invoice Number
+	Dim QINum As Long
+	'New Quotation/Invoice Number
+	Dim NewQINum as Long
+
 	QINumFormatString = "0"
 	
 	'Compute format string. E.g. if quotation number is 7 digits, format string
@@ -122,10 +132,17 @@ Sub GenerateQINumber
 	If QuoteOrInv = 0 Then
 		QICell.String = "Q" + Format(NewQINum, QINumFormatString)
 	Else
-		QICell.String = "I" + Str(NewQINum, QINumFormatString)
+		QICell.String = "I" + Format(NewQINum, QINumFormatString)
 	End If
 
 End Sub
+
+Sub WriteQINumber(QuoteOrInv As Integer)
+
+	
+
+End Sub
+
 
 Sub Test
 
