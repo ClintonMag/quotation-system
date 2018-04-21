f2090fe94ce4087853ae77dc4d4e7e4ecfb8c20c
Clinton Magano <code.clinton.magano@gmail.com>
2018-04-21 08:49:41 +0200

    Complete implementation that updates quotation or invoice number

--- quotation_system.vb
+++ quotation_system.vb
@@ -1,64 +1,112 @@
 REM  *****  BASIC  *****
 
+REM QuotationSystem
+
+REM Read quotation/invoice number from a 'QINUM' sheet
+REM Update it for the current quotation/invoice
+REM Place new quotation/invoice number in the correct cell
+REM Write new quotation/invoice, date, and client name to the
+REM 'QINUM.csv' file
+
 Option Explicit
 
-Const QROW As Integer = 6
-Const QCOL As Integer = 5
-'Number of digits in a quotation number
-Const QNUM_LENGTH = 7
+'Row and Column position of the quotation/invoice number in respective sheet
+'Row 1 in sheet = row 1 in this code
+'Col A in sheet = col 1 in this code
+Const QIROW As Integer = 7
+Const QICOL As Integer = 6
+
+'Number of digits in a quotation/invoice number
+Const QINUM_LENGTH = 7
+
+'The current spreadsheet document
 Dim Doc As Object
+
+'All sheets contained by the current spreadsheet document
 Dim AllSheets As Object
-Dim QNum As Long
+
+'The applicable working sheet, either 'Quotation' or 'Invoice'
+Dim QISheet As Object
+
+'Cell containing the quotation or invoice number
+Dim QICell as Object
+
+'Quotation/Invoice Number
+Dim QINum As Long
+
+'New Quotation/Invoice Number
+Dim NewQINum as Long
+
+'Indicate whether we are working on a quotation or an invoice
+'0 for quotation, 1 for invoice
+Dim QoutOrInv as Boolean
+
+'Index of the sheet named 'Quotation' in the ThisComponent.Sheets list
+Dim QSheetIndex as Integer
+
 
 Function Cells(sheet As Object, x As Integer, y As Integer) As Object
 	REM Simplify calls to retrieve cell position by coordinates
 	
-	Cells = sheet.getCellByPosition(x,y)
+	Cells = sheet.getCellByPosition(x-1, y-1)
 End Function
-
-Sub Main
-	REM Read quotation/invoice number from a 'QINUM' sheet
-	REM Update it for the current quotation/invoice
-	REM Place new quotation/invoice number in the correct cell
-	REM Write new quotation/invoice, date, and client name to the
-	REM 'QINUM.csv' file
 	
-	InitializeGlobals
-	GenerateQuotationNumber
-	NextQuoteNumber
+Sub MainQuotation
+	REM Entry point if working on a quotation
+	
+	QoutOrInv = 0
+	MainProcess
 
 End Sub
+
+Sub MainInvoice
+	REM Entry point if working on an invoice
 	
+	QoutOrInv = 1
+	MainProcess
+
+End Sub
+
+
+Sub MainProcess
+	REM Start the main invoice/quotation nunmber update procedure once it's
+	REM which is to be updated
+	
+	InitializeGlobals
+	GenerateQINumber
+
+End Sub
 
 Sub InitializeGlobals
 	REM Initializes the global variables
 	
-	'The current spreadsheet document
+	
 	Doc = ThisComponent
 	
-	'All sheets contained by the current spreadsheet document
 	AllSheets = Doc.Sheets
 	
-	'Sheet used for making quotations
-	QSheet = AllSheets.getByName("Quotation")
+	QSheetIndex = AllSheets.getByName("Quotation").RangeAddress.Sheet
 	
-	'Cell containing the quotation number
-	QCel = Cells(QSheet,QROW,QCOL)
+	' If working on quotation, QSheetIndex + QoutOrInv will be the "Quotation"
+	' sheet. If working on invoice,vQSheetIndex + QoutOrInv will be the
+	' "Invoice" sheet
+	QISheet = AllSheets.getByIndex(QSheetIndex + QoutOrInv)
+	
+	QICell = Cells(QISheet, QIROW, QICOL)
 	
 End Sub
 
-Sub GenerateQuotationNumber
-	REM Read the last quotation number
+Sub GenerateQINumber
+	REM Read the last quotation/invoice number, increase it by 1 and place it
+	REM in the correct cell
 
-	QNum = Val(Right(qCel.String, QNUM_LENGTH))
-
-End Sub
-
-Sub NextQuoteNumber
-	REM Calculate the next quotion number
+	QINum = Val(Right(QICell.String, QINUM_LENGTH))
+	NewQINum = QINum + 50
 	
-	QCel = Cells(QSheet,QROW,QCOL)
-	'Extract integer portion of quotation number, convert to int.
-	qCel.Value = qNum + 50
-	
+	If QuoteOrInv = 0 Then
+		QICell.String = "Q" + Str(QINum)
+	Else
+		QICell.String = "I" + Str(QINum)
+	End If
+
 End Sub
